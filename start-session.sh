fa546c5c16297670488e9866660d02fa75aaac8d
THMonster <5125031+THMonster@users.noreply.github.com>
2024-11-05 08:06:43 +0800

    update start-session.h

--- start-session.sh
+++ start-session.sh
@@ -23,7 +23,9 @@ table inet gs_steam  {
 }
 EOF
 fi
-systemd-run -p "User=${1}" -p "PAMName=login" -p "TTYPath=/dev/tty4" --wait setpriv --ambient-caps -wake_alarm /usr/share/gamescope-session-plus/gamescope-session-plus steam
+sudo chvt 4
+# systemd-run -p "User=${1}" -p "PAMName=login" -p "TTYPath=/dev/tty4" --wait setpriv --ambient-caps -wake_alarm /usr/share/gamescope-session-plus/gamescope-session-plus steam
+systemd-run -p "User=${1}" -p "UtmpIdentifier=tty4" -p "UtmpMode=user" -p "PAMName=login" -p "TTYPath=/dev/tty4" -p "TTYReset=yes" -p "TTYVHangup=yes" -p "TTYVTDisallocate=yes"  -p "NoNewPrivileges=true" -p "AmbientCapabilities=" -p "CapabilityBoundingSet=~CAP_WAKE_ALARM" --wait /usr/share/gamescope-session-plus/gamescope-session-plus steam
 check_tty=`loginctl list-sessions | grep tty2`
 if [[ "$check_tty" != "" ]]; then
 	sudo chvt 2 
